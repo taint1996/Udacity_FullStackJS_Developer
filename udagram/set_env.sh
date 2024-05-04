bb75c1473556386784d3426f7b233899b7c973d8
Tai Nguyen-Trung <tai.nguyentrung1996@gmail.com>
2024-05-04 20:33:08 +0700

    Add Final Project

---
+++ udagram/set_env.sh
@@ -0,0 +1,12 @@
+# This file is used for convenience of local development.
+# DO NOT STORE YOUR CREDENTIALS INTO GIT
+export POSTGRES_USERNAME=postgres
+export POSTGRES_PASSWORD=password
+export POSTGRES_DB=postgres
+export POSTGRES_HOST=awseb-e-sspggrcpn7-stack-awsebrdsdatabase-1jfygahypm7e.c06ayl2n8vpd.us-east-1.rds.amazonaws.com
+export AWS_BUCKET=mybk042791745847
+export AWS_REGION=us-east-1
+export AWS_DEFAULT_REGION=us-east-1
+export AWS_PROFILE=default
+export JWT_SECRET=mysecretstring
+export URL=http://udagram-env.eba-ianwm43h.us-east-1.elasticbeanstalk.com/
